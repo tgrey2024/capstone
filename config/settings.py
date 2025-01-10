5e75230b037517a8a0c11ec696e85b54ab51e20d
Tripta Grey <tripta.grey@gmail.com>
2025-01-10 20:58:57 +0000

    installed newer version of psycopg2

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,16 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.path.isfile('env.py'):
-    import env
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-tgrey2024-capstone-vwz9ad2eczu.ws.codeinstitute-ide.net','127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-tgrey2024-capstone-vwz9ad2eczu.ws.codeinstitute-ide.net',
+    '127.0.0.1',
+    '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.codeinstitute-ide.net']
