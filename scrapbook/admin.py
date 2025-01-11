df21f38dda31cb3cfb787c28590bce10bfa1d54b
Tripta Grey <tripta.grey@gmail.com>
2025-01-11 16:22:32 +0000

    add Post and Image models and install Cloudinary and urllib

--- scrapbook/admin.py
+++ scrapbook/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Scrapbook
+from .models import Scrapbook, Post, Image
 
 # Register your models here.
 admin.site.register(Scrapbook)
+
+admin.site.register(Post)
+
+admin.site.register(Image)
--- scrapbook/models.py
+++ scrapbook/models.py
@@ -1,18 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = (
-    (0, "Private"),
-    (1, "Public")
+    (0, "Draft"),
+    (1, "Private"),
+    (2, "Public")
 )
 
 # Create your models here.
 class Scrapbook(models.Model):
+    """
+    Stores a single scrapbook entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    description = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=1)
+    description = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    """
+    Stores a single post entry related to :model:`auth.User` and :model:`blog.Post`.
+    """
+    scrapbook = models.ForeignKey(Scrapbook, on_delete=models.CASCADE, related_name="post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_author")
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=-1)
+    content = models.TextField(max_length=200, blank=True)
+    approved = models.BooleanField(default=False)
+    
+
+class Image(models.Model):
+    """
+    Stores a single image entry related to :model:`scrapbook.Post`.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_image")
+    featured_image = CloudinaryField('image', default='placeholder')
+    caption = models.TextField(max_length=200, blank=True)
+    is_puzzle = models.BooleanField(default=False)
+
