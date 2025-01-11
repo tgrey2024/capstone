93060d587dd9390641a3e1576e77e3a539f92046
Tripta Grey <tripta.grey@gmail.com>
2025-01-11 19:32:12 +0000

    add django-summernote integration for Scrapbook and Post models in admin

--- scrapbook/admin.py
+++ scrapbook/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Scrapbook, Post, Image
+from django_summernote.admin import SummernoteModelAdmin
+
+
 
 # Register your models here.
-admin.site.register(Scrapbook)
+@admin.register(Scrapbook)
+class ScrapbookAdmin(SummernoteModelAdmin):
 
-admin.site.register(Post)
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+    
 admin.site.register(Image)
