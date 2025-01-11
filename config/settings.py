b0d1831f222d7093ac5e0bd6389916bbc8900416
Tripta Grey <tripta.grey@gmail.com>
2025-01-11 14:05:40 +0000

    use model to update database

--- config/settings.py
+++ config/settings.py
@@ -29,12 +29,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tgrey2024-capstone-vwz9ad2eczu.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-tgrey2024-capstone-vwz9ad2eczu.ws.codeinstitute-ide.net',
+     '127.0.0.1', '.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://*.codeinstitute-ide.net']
+    'https://*.codeinstitute-ide.net',
+    "https://*.herokuapp.com",]
 
 # Application definition
 
--- scrapbook/models.py
+++ scrapbook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = (
+    (0, "Private"),
+    (1, "Public")
+)
 
 # Create your models here.
+class Scrapbook(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
