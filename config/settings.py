ecde23834f628175de224867ca04d064b7a2b5c2
Tripta Grey <tripta.grey@gmail.com>
2025-01-13 12:50:35 +0000

    tidy up scrapbook detail template to maintain a consistent card layout throughout the app

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-tgrey2024-capstone-vwz9ad2eczu.ws.codeinstitute-ide.net',
