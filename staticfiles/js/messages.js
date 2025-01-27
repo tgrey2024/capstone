e77414d1192004862846e53e2d7d953b2d6c62fa
Tripta Grey <tripta.grey@gmail.com>
2025-01-27 01:26:16 +0000

    implement fade-out effect for notification messages and remove them from the DOM after fading

--- staticfiles/js/messages.js
+++ staticfiles/js/messages.js
@@ -1,12 +1,21 @@
+// This script hides notification messages so they start to fade out after 8 seconds
+// and will be removed from the DOM 1 second after they start fading out,
+// ie. total = 9 seconds from the page load to the message removal.
 function hideMessages() {
     setTimeout(function() {
         var messages = document.querySelectorAll('.alert');
         messages.forEach(function(msg) {
             msg.classList.remove('show');
             msg.classList.add('fade');
+            // remove the message from the DOM after it fades out
+            setTimeout(function() {
+                msg.remove();
+            }, 1000); // 1 second
         });
     }, 8000);
 }
 
 // Call the function to hide messages
-hideMessages();
+document.addEventListener('DOMContentLoaded', function() {
+    hideMessages();
+});
